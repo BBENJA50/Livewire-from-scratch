323d229265a48ebbf39c7f1c537563a9efa3dbcd
BBENJA50 <104569649+BBENJA50@users.noreply.github.com>
2024-12-02 16:04:23 +0100

    Added validation rules

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <form wire:submit="changeName()">
+    <form wire:submit="changeGreeting()">
         <div class="mt-2">
             <select type="text" class="p-4 border rounded-md bg-gray-700 text-white" wire:model.fill="greeting">
                 <option value="Hello">Hello</option>
@@ -10,13 +10,20 @@
             <input type="text" class="p-4 border rounded-md bg-gray-700 text-white" wire:model="name">
 
         </div>
+        <div>
+            @error('name')
+                <div class="text-red-500 text-sm mt-2">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
         <div class="mt-2">
             <button type="submit" class="text-white font-medium rounded-md px-4 py-2 bg-blue-600">Greet</button>
         </div>
     </form>
-    @if ($name != '')
+    @if ($greetingMessage != '')
         <div class="mt-5">
-            {{ $greeting }}, {{ $name }}!
+            {{ $greetingMessage }}
         </div>
     @endif
 </div>
